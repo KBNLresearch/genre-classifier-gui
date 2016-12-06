30607a615e3ab1517bb189813e2e48cd74afbce9
Juliette Lonij <jlonij@gmail.com>
2016-12-06 07:46:47 +0100

    Change colors

--- global.js
+++ global.js
@@ -22,7 +22,8 @@ $(function() {
 
 function markup(data) {
     var json = $.parseJSON(data);
-    var colors = ["#edf8b1","#c7e9b4","#7fcdbb","#41b6c4","#1d91c0","#225ea8","#253494","#081d58"]
+    var colors = ["#e0f3db","#ccebc5","#a8ddb5","#7bccc4","#4eb3d3","#2b8cbe","#0868ac","#084081"]
+    //var colors = ["#edf8b1","#c7e9b4","#7fcdbb","#41b6c4","#1d91c0","#225ea8","#253494","#081d58"]
     //var colors = ["#a6cee3","#1f78b4","#b2df8a","#33a02c","#fb9a99","#e31a1c","#fdbf6f","#ff7f00"]
     html = ''
     i = 0
